4052a0ac600bd1e78a3ea70a2a6223430713425a
LOstDev404 <127157866+LOstDev404@users.noreply.github.com>
2024-10-25 23:52:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,10 +70,11 @@ main_option = st.selectbox('Choose an option:', ['Open-Source', '-Changelogs-'])
 
 if main_option == 'Open-Source':
     query_params = st.experimental_get_query_params()
-    default_text = query_params.get("url", [""])[0]
+    default_text = query_params.get("input", [""])[0]
     user_input = st.text_input("Enter your text:", value=default_text)
-    st.experimental_set_query_params(url=user_input)
-    st.write(f"Current URL: `https://mcaddon-manager.streamlit.app/?url={user_input}`")
+    st.write(f"You entered: {user_input}")
+    st.experimental_set_query_params(input=user_input)
+    st.write(f"Current URL: {st.experimental_get_url()}")
 
 if main_option == '-Changelogs-':
     st.markdown("## **`Addon Manager | 0.01`:**")
