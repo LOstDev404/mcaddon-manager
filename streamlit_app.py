be85b77c1f569b35e5831da507e5b782c5a5b9e3
LOstDev404 <127157866+LOstDev404@users.noreply.github.com>
2024-10-25 22:56:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,59 @@ st.set_page_config(
     page_icon="mcaddon-logo.ico"
     
 )
+#Functions:
+#UUID Gen
+def generate_uuids():
+    return str(uuid.uuid4()), str(uuid.uuid4())
+#Manifest Modifications
+def modify_manifest(source_dir, delay, is_void_gen):
+    manifest_path = os.path.join(source_dir, 'manifest.json')
+    uuid1, uuid2 = generate_uuids()
+    with open(manifest_path, 'r') as file:
+        manifest_data = file.read()
 
-#
+    if manifest_option == '1':
+        modified_manifest_data = manifest_data.replace(
+            'packname', f'{packname1}'
+        ).replace(
+            'packdescription', f'{packdescription1}'
+        )
+    if manifest_option == '2':
+       modified_manifest_data = manifest_data.replace(
+            'packname', f'{packname2}'
+        ).replace(
+            'packdescription', f'{packdescription2}'
+        )
+    else:
+        modified_manifest_data = manifest_data.replace(
+            'packname', f'{packname2}'
+        ).replace(
+            'packdescription', f'{packdescription2}'
+        )
+        
+    modified_manifest_data = modified_manifest_data.replace('uuid1', uuid1).replace('uuid2', uuid2).replace('timedelay', str(delay))
+    
+    with open(manifest_path, 'w') as file:
+        file.write(modified_manifest_data)
+def zip_files_to_mcaddon(source_dir, output_filename):
+    with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(source_dir):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, os.path.dirname(source_dir))
+                zipf.write(file_path, arcname)
+                
+def upload_to_fileio(file_path):
+    with open(file_path, 'rb') as file:
+        response = requests.post('https://file.io', files={'file': file})
+    response_data = response.json()
+    return response_data.get('link')
+#---------------------------------------- UI Starts Here ----------------------------------------
 st.title("Dynamic Page Input Example")
 main_option = st.selectbox('Choose an option:', ['Open-Source', '-Changelogs-'])
 if main_option == 'Open-Source':
   query_params = st.experimental_get_query_params()
-  default_text = query_params.get("input", [""])[0]
+  default_text = query_params.get("url", [""])[0]
   user_input = st.text_input("Enter your text:", value=default_text)
   st.write(f"You entered: {user_input}")
   st.experimental_set_query_params(input=user_input)
