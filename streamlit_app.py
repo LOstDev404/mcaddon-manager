7cf8dc52ad23040adbf679781a05d1bd67b922cc
LOstDev404 <127157866+LOstDev404@users.noreply.github.com>
2024-10-25 23:54:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,12 +69,11 @@ st.title("Dynamic Page Input Example")
 main_option = st.selectbox('Choose an option:', ['Open-Source', '-Changelogs-'])
 
 if main_option == 'Open-Source':
-    query_params = st.experimental_get_query_params()
-    default_text = query_params.get("input", [""])[0]
-    user_input = st.text_input("Enter your text:", value=default_text)
-    st.write(f"You entered: {user_input}")
-    st.experimental_set_query_params(input=user_input)
-    st.write(f"Current URL: {st.experimental_get_url()}")
+query_params = st.experimental_get_query_params()
+default_text = query_params.get("input", [""])[0]
+user_input = st.text_input("Enter your text:", value=default_text)
+st.write(f"You entered: {user_input}")
+
 
 if main_option == '-Changelogs-':
     st.markdown("## **`Addon Manager | 0.01`:**")
